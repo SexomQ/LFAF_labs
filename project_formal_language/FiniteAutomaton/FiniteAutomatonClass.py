fb0d57e3b31c88218e0ef09ed4c403ab09a1a5dd
sexomq <sclifos-tudor@mail.ru>
2023-03-08 01:17:33 +0200

    add lab 2

--- project_formal_language/FiniteAutomaton/FiniteAutomatonClass.py
+++ project_formal_language/FiniteAutomaton/FiniteAutomatonClass.py
@@ -1,3 +1,5 @@
+from collections import deque
+
 class FiniteAutomaton:
     def __init__(self, Q, Sigma, delta, q0, F):
         self.Q = Q
@@ -15,3 +17,74 @@ class FiniteAutomaton:
             if q is None:
                 return False
         return q in self.F
+    
+    def finite_automaton_to_regular_grammar(self):
+        # Initialize the productions of the regular grammar
+        productions = {}
+        for state in self.Q:
+            productions[state] = []
+
+        # Add the productions for each transition
+        for (state, symbol, order), next_states in self.delta.items():
+            for next_state in next_states:
+                productions[state].append(symbol + next_state)
+
+        VN = self.Q
+        VT = self.Sigma
+        P = productions
+        
+        # Return the VN, VT, and P variables separately
+        return VN, VT, P
+    
+    def is_deterministic(self):
+        # Create a set to keep track of the next states for each state and input symbol
+        next_states = set()
+
+        # Iterate over all transitions
+        for (state, symbol, val), next_state in self.delta.items():
+            # Check if there is already a transition for the same state and input symbol
+            if (state, symbol) in next_states:
+                return False
+            else:
+                # Add the current transition to the set of next states
+                next_states.add((state, symbol))
+
+        # If no duplicate transitions were found, the automaton is deterministic
+        return True
+    
+    
+
+    # Define the DFA transition function
+    def nfa_to_dfa(self):
+        def epsilon_closure(state, transitions):
+            closure = {state}
+            queue = deque([state])
+            while queue:
+                current_state = queue.popleft()
+                for (s, c, t), next_ in transitions.items():
+                    if s == current_state and c == "" and next_[0] not in closure:
+                        closure.add(next_[0])
+                        queue.append(next_[0])
+            return closure
+    
+        dfa_states = []
+        dfa_transitions = {}
+        dfa_final_states = []
+        queue = deque([epsilon_closure(self.q0, self.delta)])
+        dfa_states.append(queue[0])
+        while queue:
+            current_state = queue.popleft()
+            for symbol in self.Sigma:
+                next_state = set()
+                for nfa_state in current_state:
+                    for (s, c, t), next_ in self.delta.items():
+                        if s == nfa_state and c == symbol:
+                            next_state.update(epsilon_closure(next_[0], self.delta))
+                if next_state:
+                    if next_state not in dfa_states:
+                        dfa_states.append(next_state)
+                        queue.append(next_state)
+                    dfa_transitions[(tuple(current_state), symbol)] = tuple(next_state)
+            if any(nfa_state in self.F for nfa_state in current_state):
+                dfa_final_states.append(tuple(current_state))
+        return dfa_states, self.Sigma, dfa_transitions, dfa_final_states
--- project_formal_language/Grammar/GrammarClass.py
+++ project_formal_language/Grammar/GrammarClass.py
@@ -48,3 +48,70 @@ class Grammar(FiniteAutomaton):
             delta[state] = transitions
         delta['END'] = {'END': ''}
         return delta
+    
+    def check_grammar(self):
+        type_0 = True
+        type_1 = True
+        type_2 = True
+        type_3 = True
+
+        type_3_left = True
+        type_3_right = True
+
+        for state, productions in self.P.items():
+            for production in productions:
+                # check if the grammar exist
+                for char in production:
+                    if char not in self.VT + self.VN:
+                        type_0 = False
+                        type_1 = False
+                        type_3 = False
+                        type_2 = False
+                        type_3_left = False
+                        type_3_right = False
+                        return f"The {char} is not in grammar"
+
+                for ch in state:
+                    if ch not in self.VT + self.VN:
+                        type_0 = False
+                        type_1 = False
+                        type_3 = False
+                        type_2 = False
+                        type_3_left = False
+                        type_3_right = False
+                        return f"The {char} is not in grammar"
+
+                # check if type 1 or 0
+                if (len(production) > 2 or len(state) > 1) and any([state_v in self.VN for state_v in state]):
+                    type_2, type_3 = False, False
+                    type_3_left = False
+                    type_3_right = False
+
+                # check type 1
+                if any([ch in self.VN for ch in state]):
+                    print("", end='')
+                else:
+                    type_1 = False
+                    type_2, type_3 = False, False
+                    type_3_left = False
+                    type_3_right = False
+
+                # type 2 or 3
+                if len(production) <= 2 and len(state) == 1 and type_2:
+                    if any([char_r in self.VN or len(char_r) == 0 for char_r in [production[1:]]]):
+                        print("", end='')
+                    else: type_3_right = False
+
+                    if any([char_l in self.VN or len(char_l) == 0 for char_l in [production[0:1]]]):
+                        print("", end='')
+                    else: type_3_left = False
+
+        if type_3_left or type_3_right:
+            print(f"The grammar is type 3")
+        elif type_2:
+            print(f"The grammar is type 2")
+        elif type_1:
+            print(f"The grammar is type 1")
+        elif type_0:
+            print(f"The grammar is type 0")
+
